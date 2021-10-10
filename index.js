1707cda860d7095ff3b3b0b799ca97b247f5a4d3
ggamb <glenngambardella372@gmail.com>
2021-10-10 12:06:00 -0400

    functioning HTML and CSS return withh basic boilerplate

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./src/generate-site');
 
 let nameArray = [];
 
@@ -60,191 +62,225 @@ const getInput = () => {
             return false;
           }
         }
-      },
-      {
-        type: 'confirm',
-        name: 'addMore',
-        message: 'Would you like to add another member to the team?'
       }
     ])
     .then(answer => {
-        console.log(nameArray);
-
-        nameArray.push(new Manager(answer.managerName, answer.managerID, answer.managerEmail, answer.managerOffice));
-
-
-        console.log(nameArray);
-
-        if (answer.addMore) {
-            pickEmployee();
-        }
+      //Manager object added to array
+        nameArray.push(new Manager(answer.managerName, answer.managerID, answer.managerEmail, answer.managerOffice));      
     })
     .catch(error => {
         console.log(error);
     })
 };
 
+//If user selects to add an engineer, they are prompted for information
+//and an engineer is added to the array of objects
 const addEngineer = () => {
-    return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'engineerName',
-          message: "Enter your engineer's name",
-          validate: engineerName => {
-            if (engineerName) {
-              return true;
-            } else {
-              console.log("Please enter your engineer's name!");
-              return false;
-            }
-          }
-        },
-        {
-            type: 'input',
-            name: 'engineerID',
-            message: "Enter your engineer's ID",
-            validate: engineerID => {
-              if (engineerID) {
-                return true;
-              } else {
-                console.log("Please enter your engineer's ID!");
-                return false;
-              }
-            }
-          },
-        {
-          type: 'input',
-          name: 'engineerEmail',
-          message: "Enter your engineer's email",
-          validate: engineerEmail => {
-            if (engineerEmail) {
-              return true;
-            } else {
-              console.log("Please enter your engineer's email!");
-              return false;
-            }
-          }
-        },
-        {
-          type: 'input',
-          name: 'engineerGithub',
-          message: "Enter your engineer's github",
-          validate: engineerGithub => {
-            if (engineerGithub) {
-              return true;
-            } else {
-              console.log("Please enter your engineer's github!");
-              return false;
-            }
-          }
-        },
-        {
-          type: 'confirm',
-          name: 'addMore',
-          message: 'Would you like to add another member to the team?'
-        }
-      ])
-      .then(answer => {
-        nameArray.push(new Engineer(answer.engineerName, answer.engineerID, answer.engineerEmail, answer.engineerGithub));
-        console.log(nameArray);
-
-        if (answer.addMore) {
-          pickEmployee();
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'engineerName',
+      message: "Enter your engineer's name",
+      validate: engineerName => {
+        if (engineerName) {
+          return true;
         } else {
-          console.log("Done!");
-        }
-      })
-}
-
-const addIntern = () => {
-    return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'internName',
-          message: "Enter your intern's name?",
-          validate: internName => {
-            if (internName) {
-              return true;
-            } else {
-              console.log("Please enter your intern's name!");
-              return false;
-            }
-          }
-        },
-        {
-          type: 'input',
-          name: 'internID',
-          message: "Enter your intern's ID",
-          validate: internID => {
-            if (internID) {
-              return true;
-            } else {
-              console.log("Please enter your intern's ID!");
-              return false;
-            }
-          }
-        },
-        {
-            type: 'input',
-            name: 'internEmail',
-            message: "Enter your intern's email",
-            validate: internEmail => {
-              if (internEmail) {
-                return true;
-              } else {
-                console.log("Please enter your intern's email!");
-                return false;
-              }
-            }
-          },
-        {
-          type: 'input',
-          name: 'internSchool',
-          message: "Enter your intern's school",
-          validate: internSchool => {
-            if (internSchool) {
-              return true;
-            } else {
-              console.log("Please enter your intern's school!");
-              return false;
-            }
-          }
-        },
-        {
-          type: 'confirm',
-          name: 'addMore',
-          message: 'Would you like to add another member to the team?'
-        }
-      ])
-      .then(answer => {
-        nameArray.push(new Intern(answer.internName, answer.internID, answer.internEmail, answer.internSchool));
-        console.log(nameArray);
-
-        if (answer.addMore) {
-          return pickEmployee();
-        } else {
+          console.log("Please enter your engineer's name!");
           return false;
         }
-      })
-}
+      }
+    },
+    {
+        type: 'input',
+        name: 'engineerID',
+        message: "Enter your engineer's ID",
+        validate: engineerID => {
+          if (engineerID) {
+            return true;
+          } else {
+            console.log("Please enter your engineer's ID!");
+            return false;
+          }
+        }
+      },
+    {
+      type: 'input',
+      name: 'engineerEmail',
+      message: "Enter your engineer's email",
+      validate: engineerEmail => {
+        if (engineerEmail) {
+          return true;
+        } else {
+          console.log("Please enter your engineer's email!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'engineerGithub',
+      message: "Enter your engineer's github",
+      validate: engineerGithub => {
+        if (engineerGithub) {
+          return true;
+        } else {
+          console.log("Please enter your engineer's github!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'addMore',
+      message: 'Would you like to add another member to the team?'
+    }
+  ])
+  .then(answer => {
+    //Engineer object added to array
+    nameArray.push(new Engineer(answer.engineerName, answer.engineerID, answer.engineerEmail, answer.engineerGithub));
 
-const pickEmployee = () => {
-  return inquirer.prompt([
-      {
+    if (answer.addMore) {
+      return inquirer.prompt([
+        {
           type: 'list',
           name: 'pick',
           message: 'Who would you like to add?',
           choices: ["Engineer", "Intern"]
-      }
-   ])
-   .then(answer => { 
-      if(answer.pick === "Intern") {
-          addIntern();
-      } else {
-          addEngineer();
-      }
-   })
+        }
+      ])
+      .then(answer => { 
+        if(answer.pick === "Intern") {
+          return addIntern();
+        } else {
+          return addEngineer();
+        }
+      })
+    } else {
+      return nameArray;
+    }
+  })
 }
 
+//If user selects to add an intern, they are prompted for information
+//and an intern is added to the array of objects
+const addIntern = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'internName',
+      message: "Enter your intern's name?",
+      validate: internName => {
+        if (internName) {
+          return true;
+        } else {
+          console.log("Please enter your intern's name!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'internID',
+      message: "Enter your intern's ID",
+      validate: internID => {
+        if (internID) {
+          return true;
+        } else {
+          console.log("Please enter your intern's ID!");
+          return false;
+        }
+      }
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: "Enter your intern's email",
+        validate: internEmail => {
+          if (internEmail) {
+            return true;
+          } else {
+            console.log("Please enter your intern's email!");
+            return false;
+          }
+        }
+      },
+    {
+      type: 'input',
+      name: 'internSchool',
+      message: "Enter your intern's school",
+      validate: internSchool => {
+        if (internSchool) {
+          return true;
+        } else {
+          console.log("Please enter your intern's school!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'addMore',
+      message: 'Would you like to add another member to the team?'
+    }
+  ])
+  .then(answer => {
+    //Intern object added to array
+    nameArray.push(new Intern(answer.internName, answer.internID, answer.internEmail, answer.internSchool));
 
-getInput();
+    if (answer.addMore) {
+      return inquirer.prompt([
+        {
+          type: 'list',
+          name: 'pick',
+          message: 'Who would you like to add?',
+          choices: ["Engineer", "Intern"]
+        }
+      ])
+      .then(answer => { 
+        if(answer.pick === "Intern") {
+          return addIntern();
+        } else {
+          return addEngineer();
+        }
+      })
+    } else {
+      console.log("Done!", nameArray);
+      return nameArray;
+    }
+  })
+}
+
+const pickEmployee = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'pick',
+      message: 'Who would you like to add?',
+      choices: ["Engineer", "Intern"]
+    }
+  ])
+  .then(answer => { 
+    if(answer.pick === "Intern") {
+      return addIntern();
+    } else {
+      return addEngineer();
+    }
+  })
+
+}
+
+//Initiates prompt
+getInput()
+  .then(pickEmployee)
+  .then(employeeArray => {
+    return generatePage(employeeArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(copyFileResponse => {
+    return copyFile(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ src/page-template.js
@@ -0,0 +1,68 @@
+// Create the employee cards based on user's inputs
+const generateCards = employeeArray => {
+    console.log("Generate cards function", employeeArray);
+    console.log("Generate cards function element 0", employeeArray[0]);
+
+
+    return `
+        <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Employees</h2>
+        <div class="flex-row justify-space-between">
+        ${employeeArray[0].name}
+        ${employeeArray[0].id}
+        ${employeeArray[0].email}
+        ${employeeArray[0].role}
+        ${employeeArray[0].officeNum}
+
+        ${employeeArray.map(elements => {
+            if(elements.github !== undefined) {
+                return `
+                    ${elements.github}
+                `
+            }
+        })}
+
+        ${employeeArray.map(elements => {
+            if(elements.school !== undefined) {
+                return `
+                    ${elements.school}
+                `
+            }
+        })}
+
+        </div>
+        </section>
+    `;
+};
+
+// export function to generate entire page
+module.exports = employeeArray => {
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+            <h1 class="page-title text-secondary">Project Manager</h1>
+        </div>
+        </header>
+        <main class="container my-5">
+            ${generateCards(employeeArray)}
+        </main>
+
+    </body>
+    </html>
+    `;
+};
